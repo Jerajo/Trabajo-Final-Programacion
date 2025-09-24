25f7dcd9cff8b6c3732450446c31424bd763e141
manuel <manuelmg1213@gmail.com>
2025-09-24 16:23:30 -0700

    cambios de actualizacion

--- Trabajo-Final-Programacion/Form1.Designer.cs
+++ Trabajo-Final-Programacion/Form1.Designer.cs
@@ -28,33 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            lblId = new System.Windows.Forms.Label();
-            lblNombre = new System.Windows.Forms.Label();
-            lblPrecio = new System.Windows.Forms.Label();
-            lblStock = new System.Windows.Forms.Label();
-            btnCrear = new System.Windows.Forms.Button();
-            btnActualizar = new System.Windows.Forms.Button();
-            btnEliminar = new System.Windows.Forms.Button();
-            txtID = new System.Windows.Forms.TextBox();
-            TxtNombre = new System.Windows.Forms.TextBox();
-            txtPrecio = new System.Windows.Forms.TextBox();
-            txtStock = new System.Windows.Forms.TextBox();
-            dgvProducto = new System.Windows.Forms.DataGridView();
-            Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            lblId = new Label();
+            lblNombre = new Label();
+            lblPrecio = new Label();
+            lblStock = new Label();
+            btnCrear = new Button();
+            btnActualizar = new Button();
+            btnEliminar = new Button();
+            txtID = new TextBox();
+            TxtNombre = new TextBox();
+            txtPrecio = new TextBox();
+            txtStock = new TextBox();
+            dgvProducto = new DataGridView();
+            Column1 = new DataGridViewTextBoxColumn();
+            Column2 = new DataGridViewTextBoxColumn();
+            Column3 = new DataGridViewTextBoxColumn();
+            Column4 = new DataGridViewTextBoxColumn();
+            Column5 = new DataGridViewTextBoxColumn();
+            button1 = new Button();
             ((System.ComponentModel.ISupportInitialize)dgvProducto).BeginInit();
             SuspendLayout();
             // 
             // lblId
             // 
             lblId.AutoSize = true;
-            lblId.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)0));
-            lblId.Location = new System.Drawing.Point(55, 30);
+            lblId.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblId.Location = new Point(89, 48);
+            lblId.Margin = new Padding(5, 0, 5, 0);
             lblId.Name = "lblId";
-            lblId.Size = new System.Drawing.Size(22, 18);
+            lblId.Size = new Size(36, 29);
             lblId.TabIndex = 0;
             lblId.Text = "ID";
             lblId.UseWaitCursor = true;
@@ -62,10 +64,11 @@
             // lblNombre
             // 
             lblNombre.AutoSize = true;
-            lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)0));
-            lblNombre.Location = new System.Drawing.Point(35, 66);
+            lblNombre.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblNombre.Location = new Point(57, 106);
+            lblNombre.Margin = new Padding(5, 0, 5, 0);
             lblNombre.Name = "lblNombre";
-            lblNombre.Size = new System.Drawing.Size(62, 18);
+            lblNombre.Size = new Size(101, 29);
             lblNombre.TabIndex = 1;
             lblNombre.Text = "Nombre";
             lblNombre.UseWaitCursor = true;
@@ -73,10 +76,11 @@
             // lblPrecio
             // 
             lblPrecio.AutoSize = true;
-            lblPrecio.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)0));
-            lblPrecio.Location = new System.Drawing.Point(37, 102);
+            lblPrecio.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblPrecio.Location = new Point(60, 163);
+            lblPrecio.Margin = new Padding(5, 0, 5, 0);
             lblPrecio.Name = "lblPrecio";
-            lblPrecio.Size = new System.Drawing.Size(51, 18);
+            lblPrecio.Size = new Size(83, 29);
             lblPrecio.TabIndex = 2;
             lblPrecio.Text = "Precio";
             lblPrecio.UseWaitCursor = true;
@@ -84,21 +88,22 @@
             // lblStock
             // 
             lblStock.AutoSize = true;
-            lblStock.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)0));
-            lblStock.Location = new System.Drawing.Point(42, 136);
+            lblStock.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblStock.Location = new Point(68, 218);
+            lblStock.Margin = new Padding(5, 0, 5, 0);
             lblStock.Name = "lblStock";
-            lblStock.Size = new System.Drawing.Size(47, 18);
+            lblStock.Size = new Size(73, 29);
             lblStock.TabIndex = 3;
             lblStock.Text = "Stock";
             lblStock.UseWaitCursor = true;
             // 
             // btnCrear
             // 
-            btnCrear.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)0));
-            btnCrear.Location = new System.Drawing.Point(274, 44);
-            btnCrear.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            btnCrear.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnCrear.Location = new Point(445, 45);
+            btnCrear.Margin = new Padding(5, 6, 5, 6);
             btnCrear.Name = "btnCrear";
-            btnCrear.Size = new System.Drawing.Size(90, 34);
+            btnCrear.Size = new Size(146, 54);
             btnCrear.TabIndex = 4;
             btnCrear.Text = "Crear";
             btnCrear.UseVisualStyleBackColor = true;
@@ -107,23 +112,24 @@
             // 
             // btnActualizar
             // 
-            btnActualizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)0));
-            btnActualizar.Location = new System.Drawing.Point(274, 132);
-            btnActualizar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            btnActualizar.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnActualizar.Location = new Point(445, 230);
+            btnActualizar.Margin = new Padding(5, 6, 5, 6);
             btnActualizar.Name = "btnActualizar";
-            btnActualizar.Size = new System.Drawing.Size(90, 34);
+            btnActualizar.Size = new Size(146, 54);
             btnActualizar.TabIndex = 5;
             btnActualizar.Text = "Actualizar";
             btnActualizar.UseVisualStyleBackColor = true;
             btnActualizar.UseWaitCursor = true;
+            btnActualizar.Click += btnActualizar_Click;
             // 
             // btnEliminar
             // 
-            btnEliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)0));
-            btnEliminar.Location = new System.Drawing.Point(274, 85);
-            btnEliminar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            btnEliminar.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnEliminar.Location = new Point(445, 138);
+            btnEliminar.Margin = new Padding(5, 6, 5, 6);
             btnEliminar.Name = "btnEliminar";
-            btnEliminar.Size = new System.Drawing.Size(90, 34);
+            btnEliminar.Size = new Size(146, 54);
             btnEliminar.TabIndex = 6;
             btnEliminar.Text = "Eliminar";
             btnEliminar.UseVisualStyleBackColor = true;
@@ -132,58 +138,60 @@
             // 
             // txtID
             // 
-            txtID.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)0));
-            txtID.Location = new System.Drawing.Point(106, 28);
-            txtID.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            txtID.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtID.Location = new Point(172, 45);
+            txtID.Margin = new Padding(5, 6, 5, 6);
             txtID.Name = "txtID";
             txtID.ReadOnly = true;
-            txtID.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            txtID.Size = new System.Drawing.Size(100, 24);
+            txtID.RightToLeft = RightToLeft.No;
+            txtID.Size = new Size(160, 35);
             txtID.TabIndex = 8;
             txtID.UseWaitCursor = true;
+            txtID.TextChanged += txtID_TextChanged;
             // 
             // TxtNombre
             // 
-            TxtNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)0));
-            TxtNombre.Location = new System.Drawing.Point(106, 62);
-            TxtNombre.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            TxtNombre.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            TxtNombre.Location = new Point(172, 99);
+            TxtNombre.Margin = new Padding(5, 6, 5, 6);
             TxtNombre.Name = "TxtNombre";
-            TxtNombre.Size = new System.Drawing.Size(100, 24);
+            TxtNombre.Size = new Size(160, 35);
             TxtNombre.TabIndex = 9;
             TxtNombre.UseWaitCursor = true;
             // 
             // txtPrecio
             // 
-            txtPrecio.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)0));
-            txtPrecio.Location = new System.Drawing.Point(106, 98);
-            txtPrecio.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            txtPrecio.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtPrecio.Location = new Point(172, 157);
+            txtPrecio.Margin = new Padding(5, 6, 5, 6);
             txtPrecio.Name = "txtPrecio";
-            txtPrecio.Size = new System.Drawing.Size(100, 24);
+            txtPrecio.Size = new Size(160, 35);
             txtPrecio.TabIndex = 10;
             txtPrecio.UseWaitCursor = true;
             // 
             // txtStock
             // 
-            txtStock.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)0));
-            txtStock.Location = new System.Drawing.Point(106, 132);
-            txtStock.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            txtStock.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtStock.Location = new Point(172, 211);
+            txtStock.Margin = new Padding(5, 6, 5, 6);
             txtStock.Name = "txtStock";
-            txtStock.Size = new System.Drawing.Size(100, 24);
+            txtStock.Size = new Size(160, 35);
             txtStock.TabIndex = 11;
             txtStock.UseWaitCursor = true;
             // 
             // dgvProducto
             // 
-            dgvProducto.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvProducto.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] { Column1, Column2, Column3, Column4, Column5 });
-            dgvProducto.Location = new System.Drawing.Point(40, 222);
-            dgvProducto.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            dgvProducto.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvProducto.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2, Column3, Column4, Column5 });
+            dgvProducto.Location = new Point(65, 355);
+            dgvProducto.Margin = new Padding(5, 6, 5, 6);
             dgvProducto.Name = "dgvProducto";
             dgvProducto.RowHeadersWidth = 51;
             dgvProducto.RowTemplate.Height = 24;
-            dgvProducto.Size = new System.Drawing.Size(896, 244);
+            dgvProducto.Size = new Size(1456, 390);
             dgvProducto.TabIndex = 12;
             dgvProducto.UseWaitCursor = true;
+            dgvProducto.CellContentClick += dgvProducto_CellContentClick;
             // 
             // Column1
             // 
@@ -220,12 +228,24 @@
             Column5.Name = "Column5";
             Column5.Width = 125;
             // 
+            // button1
+            // 
+            button1.Location = new Point(665, 48);
+            button1.Name = "button1";
+            button1.Size = new Size(150, 54);
+            button1.TabIndex = 13;
+            button1.Text = "insertar";
+            button1.UseVisualStyleBackColor = true;
+            button1.UseWaitCursor = true;
+            button1.Click += button1_Click;
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
-            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            BackColor = System.Drawing.Color.GhostWhite;
-            ClientSize = new System.Drawing.Size(963, 630);
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.GhostWhite;
+            ClientSize = new Size(1565, 1008);
+            Controls.Add(button1);
             Controls.Add(dgvProducto);
             Controls.Add(txtStock);
             Controls.Add(txtPrecio);
@@ -238,7 +258,8 @@
             Controls.Add(lblPrecio);
             Controls.Add(lblNombre);
             Controls.Add(lblId);
-            Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            Margin = new Padding(5, 6, 5, 6);
+            Name = "Form1";
             Text = "Form1";
             UseWaitCursor = true;
             Load += Form1_Load;
@@ -266,6 +287,7 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private Button button1;
     }
 }
 
--- Trabajo-Final-Programacion/Form1.cs
+++ Trabajo-Final-Programacion/Form1.cs
@@ -13,17 +13,17 @@ namespace Trabajo_Final_Programacion
 {
     public partial class Form1 : Form
     {
-        string connectionString = "Host=localhost:5432;Username=postgres;Password=0123.;Database=postgres";
+        string connectionString = "Host=localhost;Port=5432;Username=postgres;Password=admin.;Database=bdinventario";
         public Form1()
         {
             InitializeComponent();
         }
-        
+
         private async void Form1_Load(object sender, EventArgs e)
         {
-            
+
         }
-      
+
         private void btnCrear_Click(object sender, EventArgs e)
         {
             Producto producto = GetProducto();
@@ -80,6 +80,136 @@ namespace Trabajo_Final_Programacion
             {
                 Console.WriteLine("Error: {0}", ex.Message);
             }
+        }
+        ///actualisar a la base de datos 
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            string connString = "Host=localhost;Port=5432;Username=postgres;Password=admin;Database=bdinventario";
+
+            using (var conn = new NpgsqlConnection(connString))
+            {
+                conn.Open();
+
+                foreach (DataGridViewRow row in dgvProducto.Rows)
+                {
+                    if (row.IsNewRow) continue;
+
+                    string nombre = row.Cells[1].Value?.ToString() ?? "";
+                    decimal precio = Convert.ToDecimal(row.Cells[2].Value ?? 0);
+                    int stock = Convert.ToInt32(row.Cells[3].Value ?? 0);
+
+                    // UPDATE por nombre
+                    string queryUpdate = @"
+            UPDATE inventario.productos
+            SET precio = @precio, stock = @stock
+            WHERE nombre = @nombre";
+
+                    using (var cmd = new NpgsqlCommand(queryUpdate, conn))
+                    {
+                        cmd.Parameters.AddWithValue("precio", precio);
+                        cmd.Parameters.AddWithValue("stock", stock);
+                        cmd.Parameters.AddWithValue("nombre", nombre);
+
+                        int filasAfectadas = cmd.ExecuteNonQuery();
+
+                        // Si no existe el nombre, puedes insertar
+                        if (filasAfectadas == 0)
+                        {
+                            string queryInsert = @"
+                    INSERT INTO inventario.productos (nombre, precio, stock)
+                    VALUES (@nombre, @precio, @stock)";
+
+                            using (var cmdInsert = new NpgsqlCommand(queryInsert, conn))
+                            {
+                                cmdInsert.Parameters.AddWithValue("nombre", nombre);
+                                cmdInsert.Parameters.AddWithValue("precio", precio);
+                                cmdInsert.Parameters.AddWithValue("stock", stock);
+                                cmdInsert.ExecuteNonQuery();
+                            }
+                        }
+                    }
+                }
+
+                MessageBox.Show("Datos actualizados correctamente en PostgreSQL (por nombre)");
+            }
+        }
+
+        private void dgvProducto_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        ///agregar a la base de datos 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connString = "Host=localhost;Port=5432;Username=postgres;Password=admin;Database=bdinventario";
+
+            using (var conn = new NpgsqlConnection(connString))
+            {
+                conn.Open();
+
+                foreach (DataGridViewRow row in dgvProducto.Rows)
+                {
+                    if (row.IsNewRow) continue;
+
+                    object idValue = row.Cells[0].Value;
+                    string nombre = row.Cells[1].Value?.ToString() ?? "";
+                    decimal precio = Convert.ToDecimal(row.Cells[2].Value ?? 0);
+                    int stock = Convert.ToInt32(row.Cells[3].Value ?? 0);
+
+                    // Si existe ID válido -> UPDATE
+                    if (idValue != null && idValue != DBNull.Value && Convert.ToInt32(idValue) > 0)
+                    {
+                        int idProd = Convert.ToInt32(idValue);
+
+                        string queryUpdate = @"
+                        UPDATE inventario.productos
+                        SET nombre = @nombre, precio = @precio, stock = @stock
+                        WHERE id_producto = @id";
+
+                        using (var cmd = new NpgsqlCommand(queryUpdate, conn))
+                        {
+                            cmd.Parameters.AddWithValue("nombre", nombre);
+                            cmd.Parameters.AddWithValue("precio", precio);
+                            cmd.Parameters.AddWithValue("stock", stock);
+                            cmd.Parameters.AddWithValue("id", idProd);
+
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+                    else // Si no hay ID válido -> INSERT
+                    {
+                        string queryInsert = @"
+                    INSERT INTO inventario.productos (nombre, precio, stock)
+                    VALUES (@nombre, @precio, @stock)
+                    RETURNING id_producto";
+
+                        using (var cmd = new NpgsqlCommand(queryInsert, conn))
+                        {
+                            cmd.Parameters.AddWithValue("nombre", nombre);
+                            cmd.Parameters.AddWithValue("precio", precio);
+                            cmd.Parameters.AddWithValue("stock", stock);
+
+                            int newId = (int)cmd.ExecuteScalar();
+                            row.Cells[0].Value = newId; // Asigna el ID al DataGridView
+                        }
+                    }
+                }
+
+                MessageBox.Show("Datos guardados correctamente en PostgreSQL");
+            }
+        }
+
+        
+        private void txtID_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
